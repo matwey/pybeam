dff4e16dfead1884d34c5ab99ca3beaa6b841b75
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2013-04-29 20:51:45 +0400

    Add @property atoms

--- pybeam/beam_file.py
+++ pybeam/beam_file.py
@@ -4,4 +4,12 @@ class BeamFile(object):
 	def __init__(self, filename):
 		self._tree = beam.parse(file(filename,"r").read())
 
-	
+	def selectChunkByName(self, name):
+		for c in self._tree.chunk:
+			if c.chunk_name == name:
+				return c
+		raise KeyError(name)
+
+	@property
+	def atoms(self):
+		return self.selectChunkByName("Atom").payload.atom
