c721656fac8fce4334751931a957191d9768aebd
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-12-25 18:45:06 +0300

    Replace spaces with tabs in setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,21 @@ name="pybeam"
 version="0.4.1"
 
 setup(name=name,
-      version=version,
-      description='Python module to parse Erlang BEAM files',
-      url='http://github.com/matwey/pybeam',
-      author='Matwey V. Kornilov',
-      author_email='matwey.kornilov@gmail.com',
-      license='MIT',
-      packages=['pybeam'],
-      test_suite = 'test',
-      install_requires=['construct>=2.8,<2.9', 'six'],
-      command_options={
-            'build_sphinx': {
-                'project': ('setup.py', name),
-                'version': ('setup.py', version),
-                'release': ('setup.py', version),
-                'source_dir': ('setup.py', 'doc')}},
-      zip_safe=False)
+	version=version,
+	description='Python module to parse Erlang BEAM files',
+	url='http://github.com/matwey/pybeam',
+	author='Matwey V. Kornilov',
+	author_email='matwey.kornilov@gmail.com',
+	license='MIT',
+	packages=['pybeam'],
+	test_suite='test',
+	install_requires=['construct>=2.8,<2.9', 'six'],
+	command_options={
+		'build_sphinx': {
+			'project': ('setup.py', name),
+			'version': ('setup.py', version),
+			'release': ('setup.py', version),
+			'source_dir': ('setup.py', 'doc')
+		}
+	},
+	zip_safe=False)
